12097a3eb5a6b25bd316586b398b5ee8b14951f8
KwongTN <tungnan5636@gmail.com>
2019-10-25 18:34:10 +0800

    Connected to mySQL server

--- src/web/server.js
+++ src/web/server.js
@@ -3,14 +3,34 @@ var session = require("cookie-session");
 var bodyParser = require("body-parser");
 var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
+// Database connection 
+const mysqlx = require('@mysql/xdevapi');
+
+
+// 8081 uses x-Protocol, while 8082 uses the old authentication method
+const config = {
+    password: 'password',
+    user: 'root',
+    host: 'localhost',
+    port: 8081
+};
+
+mysqlx.getSession(config)
+.then(session => {
+    console.log(session.inspect());
+})
+.catch(console.error());
+
+// Application 
 var app = express();
 
 var appName = "club-man";
 
-// Declaring application 
 app.use(session({secure: true, secret: "someKey"}))
 
 .use("/login", function(req, res){
+
+.use("/", function(req, res){
     var now = new Date();
     res.render("login.ejs", {
         appName: appName,
@@ -21,7 +41,6 @@ app.use(session({secure: true, secret: "someKey"}))
 .post("/login",  urlEncodedParser, function(req, res){
     var login = req.body.username;
     var pass = req.body.pass;
-
     
 })
 
