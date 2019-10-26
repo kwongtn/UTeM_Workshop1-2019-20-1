53b11fa492c050423dec4cc3856c023c48cece15
KwongTN <tungnan5636@gmail.com>
2019-10-27 00:28:30 +0800

    Get Data from DB successful
    - Its is decided that for every page request there will be a new connection to the database. And will close connection once done.
    - Connection closing will be done later on

--- src/web/server.js
+++ src/web/server.js
@@ -1,7 +1,8 @@
+"use strict";
 var express = require("express");
 var session = require("cookie-session");
 var bodyParser = require("body-parser");
-var urlEncodedParser = bodyParser.urlencoded({extended: false});
+var urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
 // Database connection 
 const mysqlx = require('@mysql/xdevapi');
@@ -15,42 +16,59 @@ const config = {
     port: 8081
 };
 
-mysqlx.getSession(config)
-.then(session => {
-    console.log(session.inspect());
-})
-.catch(console.error());
-
 // Application 
 var app = express();
 
 var appName = "club-man";
 
-app.use(session({secure: true, secret: "someKey"}))
+app.use(session({ secure: true, secret: "someKey" }))
 
-.get("/", function(req, res){
-    var now = new Date();
-    res.render("login.ejs", {
-        appName: appName,
-        time: now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + "." + now.getMilliseconds()
-    });
-})
+    .get("/", function (req, res) {
+        var now = new Date();
+        res.render("login.ejs", {
+            appName: appName,
+            time: now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + "." + now.getMilliseconds()
+        });
+    })
 
-.post("/login/",  urlEncodedParser, function(req, res){
-    var login = req.body.username;
-    var pass = req.body.pass;
+    .post("/login/", urlEncodedParser, function (req, res) {
+        var login = req.body.username;
+        var pass = req.body.pass;
 
-    console.log("Login: " + req.body.username);
-    console.log("Password: " + req.body.pass);
-    
-    res.redirect("/");
-})
+        console.log("Login: " + req.body.username);
+        console.log("Password: " + req.body.pass);
 
-.get("/home", function(req, res){
-    res.render("home.ejs",{
-        appName: appName
-    });
-})
+        res.redirect("/home");
+    })
 
-.listen(8080)
-;
+    .get("/home", function (req, res) {
+        res.render("home.ejs", {
+            appName: appName
+        });
+    })
+
+    .get("/listUsers", function (req, res) {
+        var userList = mysqlx.getSession(config)
+            .then(session => {
+                return session.getSchema("CLUB-MAN").getTable("USER");
+            })
+            .then(table => {
+                return table.select(["userID", "engName", "email", "faculty"])
+                    .bind('fac', 'FTMK')
+                    .execute();
+            })
+            .then(output => {
+                console.log(output.fetchAll());
+                console.log("==================");
+            });
+
+        res.render("listUsers.ejs", {
+
+        })
+
+    })
+
+
+    .listen(8080)
+    ;
+
